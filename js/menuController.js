0d6e75dd3920d31fa014728535508968324a9c5e
scarma <scarma@hotmail.it>
2016-10-22 14:47:48 +0200

    Create menuController.js

---
+++ js/menuController.js
@@ -0,0 +1,34 @@
+var app = angular.module("ScarmaGames", []);
+app.controller("menuController", ['$scope', function($scope) {
+  $scope.items = [{
+    label: "Warp bird",
+    link: "Warp bird",
+    desc : "A game about birds"
+  }, {
+    label: "Star In Black",
+    link: "Star In Black",
+    desc : "A game about stars"
+  },{
+    label: "Rob - The Talking Robot",
+    link: "Rob",
+    desc : "A game about a talking robot"
+  },{
+    label: "Chat & Shoot",
+    link: "Chat & Shoot",
+    desc : "A game about chatting and shooting laser beams"
+  },{
+    label: "The Tutorial To Play This Game",
+    link: "The Tutorial To Play This Game",
+    desc : "What happens if the tutorial becomes the game?"+"<br>"+
+"The objective of the tutorial is to play this game."+"<br>"+
+"The game is quite a short game and is a little idea made concrete."+"<br>"+
+"Game made in less than 10 hours from scratch using Construct 2, Audacity and Photofiltre."+"<br>"+
+"Ending music generated with This Exquisite Music Engine. HINT: Words are meaningful."+"<br>"
+  },{
+    label: "Webcam Effects",
+    link: "Webcam Effects",
+    desc : "A game about using a webcam"
+  }
+  
+  ];
+}]);
