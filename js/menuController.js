f57d3d24a8fa203e38918f4fb879b60eded17bf8
scarma <scarma@hotmail.it>
2016-10-23 22:51:52 +0200

    Update menuController.js

--- js/menuController.js
+++ js/menuController.js
@@ -6,6 +6,7 @@ app.config(function($locationProvider) {
 app.controller("menuController", ['$scope', '$window',  function($scope,$window) {
   
 	$scope.play=function(gameUrl){
+		if (!/^(f|ht)tps?:\/\//i.test(gameUrl)) {
 		$window.location.href= gameUrl+ '/index.html';
 		
 	//$location.path( gameUrl+ '/index.html');
@@ -13,7 +14,7 @@ app.controller("menuController", ['$scope', '$window',  function($scope,$window)
 var linebreak="\n";	
   $scope.items = [{
     label: "Warp Bird",
-    link: "Warp Bird",
+    link: "http://scarma.altervista.org/WarpBird/index.html",
     desc : "A Bird Can Destroy"+linebreak+
 "You are a cute little bird who can't fly. But you can collect bombs and hammers."+linebreak+
 	  "Enter The Warp Zone!"+linebreak+
